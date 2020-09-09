21e2f8b6d8c1ac7a53da47f923383927138ff6a7
LordOfTheLost <48102549+LordOfTheLost@users.noreply.github.com>
2020-09-09 20:28:51 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,6 +17,7 @@
 # 
 # Please maintain this if you use this script or any part of it
 #
+
 FDEVICE="dipper"
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 export PLATFORM_VERSION="16.1.0"
@@ -45,7 +46,7 @@ export OF_CHECK_OVERWRITE_ATTEMPTS=1
 export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
 export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
 
-export OF_PATCH_AVB20=1
+#export OF_PATCH_AVB20=0
 
 # Use system (ROM) fingerprint where available
 export OF_USE_SYSTEM_FINGERPRINT=1
@@ -53,7 +54,7 @@ export OF_USE_SYSTEM_FINGERPRINT=1
 # Settings R11
 export FOX_R11=1
 #export FOX_ADVANCED_SECURITY=
-#export OF_USE_TWRP_SAR_DETECT=
+export OF_USE_TWRP_SAR_DETECT=1
 export OF_QUICK_BACKUP_LIST="/boot;/data;/system;/vendor;"
 export FOX_VERSION=R11
 export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
@@ -67,6 +68,8 @@ export OF_SCREEN_H=2248
 export OF_STATUS_H=80
 export OF_STATUS_INDENT_LEFT=48
 export OF_STATUS_INDENT_RIGHT=48
+#export OF_NO_SPLASH_CHANGE=1
+#export OF_DISABLE_EXTRA_ABOUT_PAGE=1
 
 # Other
 #export FOX_USE_TAR_BINARY=1
@@ -76,8 +79,12 @@ export OF_FLASHLIGHT_ENABLE=1
 #export OF_KEEP_FORCED_ENCRYPTION=1
 #export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
 
+# Compiler 
+#export OF_DISABLE_KEYMASTER2=1
+#export OF_LEGACY_SHAR512=1
+
 # Fox Version
-export FOX_VERSION=R11-Build4
+export FOX_VERSION=R11.0-Build5
 
 # Let's see what are our build VARs
 if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
@@ -90,4 +97,3 @@ fi
 add_lunch_combo omni_"$FDEVICE"-eng
 
 fi
-#
