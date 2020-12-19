a8a0bf54b7853e064b3170d56e98dcb0b5ca0fe8
LordOfTheLost <48102549+LordOfTheLost@users.noreply.github.com>
2020-12-19 20:16:49 +0200

    Update Build_OF.sh

--- Build_OF.sh
+++ Build_OF.sh
@@ -1,46 +1,58 @@
 # configure some default settings for the build
+VOF="$( date +"%d.%m" ).20-(10)"
 Default_Settings() {
 FDEVICE="dipper"
+# Other Settings
 export PLATFORM_VERSION="16.1.0"
 export PLATFORM_SECURITY_PATCH="2099-12-31"
 export TW_DEFAULT_LANGUAGE="en"
+export FOX_R11=1
+#export FOX_ADVANCED_SECURITY=0
+export OF_USE_TWRP_SAR_DETECT=1
+#export OF_VANILLA_BUILD=1
 export OF_CLASSIC_LEDS_FUNCTION=1
 export OF_USE_MAGISKBOOT=1
 export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-export FOX_USE_LZMA_COMPRESSION=1
-export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-export OF_OTA_RES_DECRYPT=1
-export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-export FOX_USE_BASH_SHELL=1
-export FOX_ASH_IS_BASH=1
-export FOX_USE_NANO_EDITOR=1
 export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI=1 # if you disable this, then enable the next line
 #export OF_NO_MIUI_PATCH_WARNING=1
-export FOX_REPLACE_BUSYBOX_PS=1
 export OF_USE_NEW_MAGISKBOOT=1
-export OF_KEEP_DM_VERITY=1  # enabled for R10.1
-export OF_DISABLE_FORCED_ENCRYPTION=1  # enabled for R10.1 (A10)
-export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
 export OF_CHECK_OVERWRITE_ATTEMPTS=1
+export FOX_RESET_SETTINGS=1
+export OF_FLASHLIGHT_ENABLE=1
+#export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+export OF_SUPPORT_PRE_FLASH_SCRIPT=1
+
+#BACKUP
+export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
+export OF_QUICK_BACKUP_LIST="/boot;/data;/system_root;/vendor;"
+
+# Files
+export FOX_REPLACE_BUSYBOX_PS=1
+export FOX_REMOVE_AAPT=1
+export FOX_USE_BASH_SHELL=1
+export FOX_ASH_IS_BASH=1
+#export FOX_USE_NANO_EDITOR=1
+#export FOX_USE_TAR_BINARY=1
+#export FOX_USE_ZIP_BINARY=1
+#export FOX_DELETE_MAGISK_ADDON=1
 
 # OTA for custom ROMs
 export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
 export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-
-#export OF_PATCH_AVB20=0
-
-# Use system (ROM) fingerprint where available
-export OF_USE_SYSTEM_FINGERPRINT=1
-
-# Settings R11
-export FOX_R11=1
-#export FOX_ADVANCED_SECURITY=
-export OF_USE_TWRP_SAR_DETECT=1
-export OF_QUICK_BACKUP_LIST="/boot;/data;/system_root;/vendor;"
 export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
 export FOX_DELETE_AROMAFM=1
 export FOX_DELETE_INITD_ADDON=1
-#export OF_VANILLA_BUILD=
+
+# Encryption
+export OF_PATCH_AVB20=1
+export OF_OTA_RES_DECRYPT=1
+export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+export OF_KEEP_DM_VERITY=1
+export OF_DISABLE_FORCED_ENCRYPTION=1
+#export OF_KEEP_FORCED_ENCRYPTION=1
+
+# Use system (ROM) fingerprint where available
+#export OF_USE_SYSTEM_FINGERPRINT=1
 
 # UI
 export OF_CLOCK_POS=0
@@ -52,26 +64,22 @@ export OF_STATUS_INDENT_RIGHT=48
 #export OF_NO_SPLASH_CHANGE=1
 #export OF_DISABLE_EXTRA_ABOUT_PAGE=1
 
-# Other
-#export FOX_USE_TAR_BINARY=1
-#export FOX_USE_ZIP_BINARY=1
-export FOX_RESET_SETTINGS=1
-export OF_FLASHLIGHT_ENABLE=1
-#export OF_KEEP_FORCED_ENCRYPTION=1
-#export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-#export FOX_DELETE_MAGISK_ADDON=1
+# DEBUG MODE
+export FOX_INSTALLER_DEBUG_MODE=0
 
-# Compiler 
+# Compiler
+export FOX_USE_LZMA_COMPRESSION=1
+export ALLOW_MISSING_DEPENDENCIES=true
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+export LC_ALL="C"
 #export OF_DISABLE_KEYMASTER2=1
 #export OF_LEGACY_SHAR512=1
 
 # Fox Version
-export FOX_VERSION=R11.0-Build10  
+export FOX_VERSION=R11.0-$VOF
+export FOX_BUILD_TYPE=Weekly
 
-# Other Compiler Vars
-export ALLOW_MISSING_DEPENDENCIES=true
-export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-export LC_ALL="C"
+# MAINTAINER
 export OF_MAINTAINER="Lord Of The Lost"
 }
 
@@ -80,6 +88,8 @@ Build() {
 Default_Settings
 # compile it
 . build/envsetup.sh
+add_lunch_combo omni_$FDEVICE-eng
+add_lunch_combo omni_$FDEVICE-userdebug
 lunch omni_$FDEVICE-eng && mka recoveryimage
 }
 
