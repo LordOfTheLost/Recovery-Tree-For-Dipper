49be3a5fe2df10eec0ec47d9e7cdfde39d982a97
LordOfTheLost <48102549+LordOfTheLost@users.noreply.github.com>
2021-05-12 02:26:09 +0300

    Add files via upload

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,7 +49,6 @@ LZMA_RAMDISK_TARGETS := recovery
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA84000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 androidboot.selinux=permissive
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
@@ -129,7 +128,6 @@ BOARD_SUPPRESS_SECURE_ERASE := true
 
 # Extras
 TW_IGNORE_MISC_WIPE_DATA := true
-BOARD_SUPPRESS_SECURE_ERASE := true
 
 # Debug
 TWRP_INCLUDE_LOGCAT := true
