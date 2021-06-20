c4970d53e27be1d86dde0b1348013d07b4c26794
LordOfTheLost <48102549+LordOfTheLost@users.noreply.github.com>
2021-06-20 19:33:06 +0300

    Add files via upload

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,7 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := device/xiaomi/dipper
+LOCAL_PATH := device/dipper
 
 # For building with minimal manifest
 # ALLOW_MISSING_DEPENDENCIES := true
@@ -33,7 +33,7 @@ TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a9
-# TARGET_BOARD_SUFFIX := _64
+TARGET_BOARD_SUFFIX := _64
 TARGET_USES_64_BIT_BINDER := true
 
 ENABLE_CPUSETS := true
@@ -42,10 +42,11 @@ ENABLE_SCHEDBOOST := true
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := sdm845
 TARGET_NO_BOOTLOADER := true
-# TARGET_USES_UEFI := true
+TARGET_USES_UEFI := true
 
 # lzma
-LZMA_RAMDISK_TARGETS := recovery
+LZMA_RAMDISK_TARGETS := boot,recovery
+BOARD_NEEDS_LZMA_MINIGZIP := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA84000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 androidboot.selinux=permissive
@@ -73,20 +74,17 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 57453555712
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_PARTITION_SIZE := 788529152
 
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-
 # Workaround for error copying vendor files to recovery ramdisk
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Recovery
 TARGET_OTA_ASSERT_DEVICE := dipper
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_HAS_NO_SELECT_BUTTON := true
+# BOARD_HAS_LARGE_FILESYSTEM := true
+# BOARD_HAS_NO_SELECT_BUTTON := true
 
 # Android Verified Boot
-BOARD_AVB_ENABLE := false
-BOARD_BUILD_DISABLED_VBMETAIMAGE := true
+# BOARD_AVB_ENABLE := false
+# BOARD_BUILD_DISABLED_VBMETAIMAGE := true
 
 # TWRP specific build flags
 TW_THEME := portrait_hdpi
@@ -96,24 +94,22 @@ TARGET_RECOVERY_QCOM_RTC_FIX := true
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_DEFAULT_BRIGHTNESS := 420
-TW_DEFAULT_LANGUAGE := en
-TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_EXTRA_LANGUAGES := true
-TW_INCLUDE_NTFS_3G := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_MAX_BRIGHTNESS := 1023
-PLATFORM_SECURITY_PATCH := 2099-12-31
+# TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXTRA_LANGUAGES := false
+# TW_INCLUDE_NTFS_3G := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery.fstab
-AB_OTA_UPDATER := false
+# AB_OTA_UPDATER := false
 TW_USE_QCOM_HAPTICS_VIBRATOR := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_USE_TOOLBOX := true
 
-# exFAT FS Support
-TW_INCLUDE_FUSE_EXFAT := true
-
-# NTFS Support
-TW_INCLUDE_FUSE_NTFS := true
+# File Systems
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+# TW_INCLUDE_FUSE_NTFS := true
+# TW_INCLUDE_FUSE_EXFAT := true
 
 # Crypto
 TW_INCLUDE_CRYPTO := true
@@ -123,8 +119,8 @@ TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
 
 # System as root
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
-BOARD_SUPPRESS_SECURE_ERASE := true
+# BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+# BOARD_SUPPRESS_SECURE_ERASE := true
 
 # Extras
 TW_IGNORE_MISC_WIPE_DATA := true
--- Build_OF.sh
+++ Build_OF.sh
@@ -1,12 +1,17 @@
-# Configure some default settings for the build
-MMVOF="$1"; FDEVICE="dipper"; VOF="$( date +"%d.%m" ).21-(18)"; OFKP="prebuilt/Image.tar.xz"; FONTXML="scripts/OrangeFox/bootable/recovery/gui/theme/portrait_hdpi/themes/font.xml"; ADVANCEDXML="scripts/OrangeFox/bootable/recovery/gui/theme/portrait_hdpi/pages/advanced.xml"; INSTALLER="scripts/OrangeFox/vendor/recovery/installer/META-INF/com/google/android/update-binary"
-sudo chmod -R 777 scripts/OrangeFox/vendor/recovery
+BRAND="xiaomi"; FDEVICE="dipper"; VOF="$( date +"%d.%m" ).21-(19)"; OFKP="prebuilt/Image.tar.xz"
+RECOVERY="scripts/OrangeFox/vendor/recovery"
+FOXFILES="$RECOVERY/FoxFiles"
+FOXDEVICE="scripts/OrangeFox/device"
+FONTXML="scripts/OrangeFox/bootable/recovery/gui/theme/portrait_hdpi/themes/font.xml"
+ADVANCEDXML="scripts/OrangeFox/bootable/recovery/gui/theme/portrait_hdpi/pages/advanced.xml"
+INSTALLER="$RECOVERY/installer/META-INF/com/google/android/update-binary"
+sudo chmod -R 777 $RECOVERY
 
 Patch_OF_Settings() {
-if [ -f scripts/OrangeFox/vendor/recovery/ADVANCEDXML ]; then touch scripts/OrangeFox/vendor/recovery/ADVANCEDXML; else sed -i "320,356 d" $ADVANCEDXML; touch scripts/OrangeFox/vendor/recovery/ADVANCEDXML; fi
+if [ -f $RECOVERY/ADVANCEDXML ]; then touch $RECOVERY/ADVANCEDXML; else sed -i "320,356 d" $ADVANCEDXML; touch $RECOVERY/ADVANCEDXML; fi
 # sed -i "s/<placement x=\"%col1_x_caption%\" y=\"%row3_1a_y%\"\/>/<placement x=\"0\" y=\"0\"\/>/g" $ADVANCEDXML
-# sed -i "s/<placement x=\"%col1_x_caption%\" y=\"%row5_2_y%\"\/>/<placement x=\"%col1_x_caption%\" y=\"%row3_1a_y%\"\/>/g" $ADVANCEDXML
-sed -i "s/<placement x=\"0\" y=\"%row5_3_y%\" w=\"%screen_w%\" h=\"%bl_h4%\"\/>/<placement x=\"0\" y=\"%row3_2a_y%\" w=\"%screen_w%\" h=\"%bl_h3%\"\/>/g" $ADVANCEDXML
+sed -i "s/<placement x=\"%col1_x_caption%\" y=\"%row5_2_y%\"\/>/<placement x=\"%col1_x_caption%\" y=\"%row4_1a_y%\"\/>/g" $ADVANCEDXML
+sed -i "s/<placement x=\"0\" y=\"%row5_3_y%\" w=\"%screen_w%\" h=\"%bl_h4%\"\/>/<placement x=\"0\" y=\"%row4_2a_y%\" w=\"%screen_w%\" h=\"%bl_h4%\"\/>/g" $ADVANCEDXML
 # sed -i "s/Roboto/GoogleSans/g" $FONTXML
 # sed -i "s/value=\"n\"/value=\"s\"/g" $FONTXML
 sed -i "s/<condition var1=\"of_hide_app_hint\" op=\"!=\" var2=\"1\"\/>/<condition var1=\"of_hide_app_hint\" op=\"!=\" var2=\"0\"\/>/g" $ADVANCEDXML
@@ -16,16 +21,12 @@ sed -i "/<condition var1=\"utils_show\" var2=\"1\"\/>/d" $ADVANCEDXML
 }
 
 Default_Settings() {
-if [ -f scripts/OrangeFox/device/xiaomi/$FDEVICE/maintainer.png ]; then cp -f scripts/OrangeFox/device/xiaomi/$FDEVICE/maintainer.png scripts/OrangeFox/bootable/recovery/gui/theme/portrait_hdpi/images/Default/About; rm -f scripts/OrangeFox/device/xiaomi/$FDEVICE/maintainer.png; fi
-if [ -f scripts/OrangeFox/device/xiaomi/$FDEVICE/busybox ]; then cp -f scripts/OrangeFox/device/xiaomi/$FDEVICE/busybox scripts/OrangeFox/vendor/recovery/Files; rm -f scripts/OrangeFox/device/xiaomi/$FDEVICE/busybox; fi
-if [ -f scripts/OrangeFox/vendor/recovery/FoxFiles/GoogleSans.zip ]; then rm -f scripts/OrangeFox/vendor/recovery/FoxFiles/GoogleSans.zip; fi
-if [ -f scripts/OrangeFox/vendor/recovery/FoxFiles/SubstratumRescue.zip ]; then rm -f scripts/OrangeFox/vendor/recovery/FoxFiles/SubstratumRescue.zip; fi
-if [ -f scripts/OrangeFox/vendor/recovery/FoxFiles/SubstratumRescue_Legacy.zip ]; then rm -f scripts/OrangeFox/vendor/recovery/FoxFiles/SubstratumRescue_Legacy.zip; fi
-if [ -f scripts/OrangeFox/vendor/recovery/FoxFiles/OF_initd.zip ]; then rm -f scripts/OrangeFox/vendor/recovery/FoxFiles/OF_initd.zip; fi
-if [ -d scripts/OrangeFox/vendor/recovery/FoxFiles/AromaFM ]; then rm -rf scripts/OrangeFox/vendor/recovery/FoxFiles/AromaFM; fi
-if [ -f scripts/OrangeFox/device/xiaomi/$FDEVICE/unrootmagisk.zip ]; then cp -f scripts/OrangeFox/device/xiaomi/$FDEVICE/unrootmagisk.zip scripts/OrangeFox/vendor/recovery/FoxFiles/unrootmagisk.zip; rm -f scripts/OrangeFox/device/xiaomi/$FDEVICE/unrootmagisk.zip; fi
-#if [ -f scripts/OrangeFox/device/xiaomi/$FDEVICE/recovery/root/sbin/runatboot.sh ]; then cp -f scripts/OrangeFox/device/xiaomi/$FDEVICE/recovery/root/sbin/runatboot.sh scripts/OrangeFox/vendor/recovery/FoxFiles; fi
-cd scripts/OrangeFox; if [ -f device/xiaomi/$FDEVICE/$OFKP ]; then tar -xf device/xiaomi/$FDEVICE/$OFKP -C device/xiaomi/$FDEVICE/prebuilt; rm -f device/xiaomi/$FDEVICE/$OFKP; fi
+if [ -f $FOXDEVICE/$FDEVICE/maintainer.png ]; then cp -f $FOXDEVICE/$FDEVICE/maintainer.png scripts/OrangeFox/bootable/recovery/gui/theme/portrait_hdpi/images/Default/About; rm -f $FOXDEVICE/$FDEVICE/maintainer.png; fi
+if [ -f $FOXDEVICE/$FDEVICE/busybox ]; then cp -f $FOXDEVICE/$FDEVICE/busybox $RECOVERY/Files; fi
+for f in GoogleSans.zip SubstratumRescue.zip SubstratumRescue_Legacy.zip OF_initd.zip AromaFM; do if [ -f $FOXFILES/$f.zip ]; then rm -rf $FOXFILES/$f; fi; done
+if [ -f $FOXDEVICE/$FDEVICE/unrootmagisk.zip ]; then cp -f $FOXDEVICE/$FDEVICE/unrootmagisk.zip $FOXFILES/unrootmagisk.zip; rm -f $FOXDEVICE/$FDEVICE/unrootmagisk.zip; fi
+#if [ -f $FOXDEVICE/$FDEVICE/recovery/root/sbin/runatboot.sh ]; then cp -f $FOXDEVICE/$FDEVICE/recovery/root/sbin/runatboot.sh $FOXFILES; fi
+cd scripts/OrangeFox; if [ -f device/$FDEVICE/$OFKP ]; then tar -xf device/$FDEVICE/$OFKP -C device/$FDEVICE/prebuilt; rm -f device/$FDEVICE/$OFKP; fi
 }
 
 Default_Fox_Vars() {
@@ -36,7 +37,7 @@ export TW_DEFAULT_LANGUAGE="en"
 #export FOX_R11=1; #DEPCRECATED!
 #export FOX_ADVANCED_SECURITY=0; # This forces ADB and MTP to be disabled until after you enter the recovery (ie, until after all decryption/recovery passwords are successfully entered)
 export OF_USE_TWRP_SAR_DETECT=0; # Blyad Prosto
-export OF_SUPPORT_PRE_FLASH_SCRIPT=1; # Support running a script before flashing zips (other than ROMs). The script must be called /sbin/fox_pre_flash - and you need to copy it there yourself
+export OF_SUPPORT_PRE_FLASH_SCRIPT=0; # Support running a script before flashing zips (other than ROMs). The script must be called /sbin/fox_pre_flash - and you need to copy it there yourself
 #export OF_VANILLA_BUILD=1
 export OF_CLASSIC_LEDS_FUNCTION=0; # Use the old R9.x Leds function
 export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1; # If this is set, this script will also automatically set OF_USE_MAGISKBOOT to 1
@@ -47,7 +48,7 @@ export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI=1 # if you disable this, then enable 
 export OF_CHECK_OVERWRITE_ATTEMPTS=1; # Check for attempts by a ROM's installer to overwrite OrangeFox with another recovery
 export FOX_RESET_SETTINGS=1
 export OF_FLASHLIGHT_ENABLE=1
-export FOX_DISABLE_APP_MANAGER=0
+export FOX_DISABLE_APP_MANAGER=1
 #export OF_NO_TREBLE_COMPATIBILITY_CHECK=1; # Disable checking for compatibility.zip in ROMs
 export OF_RUN_POST_FORMAT_PROCESS=1
 
@@ -60,14 +61,14 @@ export FOX_USE_GREP_BINARY=0
 export FOX_DELETE_AROMAFM=1
 export FOX_DELETE_INITD_ADDON=1
 export FOX_REPLACE_BUSYBOX_PS=1
-#export FOX_REMOVE_AAPT=1; # Used for FOX_DISABLE_APP MANAGER if on enable
-export FOX_USE_BASH_SHELL=1
-export FOX_ASH_IS_BASH=1
-#export FOX_USE_NANO_EDITOR=1
-#export FOX_USE_TAR_BINARY=1
+export FOX_REMOVE_AAPT=1; # Used for FOX_DISABLE_APP MANAGER if on enable
+export FOX_USE_BASH_SHELL=0
+export FOX_ASH_IS_BASH=0
+export FOX_USE_NANO_EDITOR=0
+export FOX_USE_TAR_BINARY=0
 #export FOX_USE_ZIP_BINARY=1; # OBSOLETE!
 #export FOX_DELETE_MAGISK_ADDON=1
-export FOX_USE_XZ_UTILS=1
+export FOX_USE_XZ_UTILS=0
 
 # OTA for custom ROMs
 export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1; #This setting is incompatible with OF_DISABLE_MIUI_SPECIFIC_FEATURES/OF_TWRP_COMPATIBILITY_MODE/OF_VANILLA_BUILD
@@ -108,10 +109,10 @@ export LC_ALL="C"
 #export FOX_JAVA8_PATH="/usr/lib/jvm/java-8-openjdk/jre/bin/java
 
 # Fox Version
-export FOX_VERSION=R11.1-$VOF$MMVOF
+export FOX_VERSION=R11.1-$VOF
 export FOX_BUILD_TYPE=Monthly
 
-# MAINTAINER
+# CUMTAINER
 export OF_MAINTAINER="Lord Of The Lost"
 }
 
@@ -128,6 +129,4 @@ add_lunch_combo omni_$FDEVICE-userdebug
 lunch omni_$FDEVICE-eng && mka recoveryimage
 }
 
-# --- main --- #
 Build
-#
